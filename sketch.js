5ffc8eb0a560cb685b35501ae81b2f220fef9be0
nclufo <naomi.clough13@gmail.com>
2025-01-26 15:33:26 +0000

    fixed flock

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
 const flock = [];
+let alignSlider, cohesionSlider, separationSlider;
 
 function setup() {
-  createCanvas(400, 400);
-  for (let i = 0; i < 200; i++){
-  flock.push(new Boid())}
+  createCanvas(600, 400);
+  alignSlider = createSlider(0, 2, 1.5, 0.1);
+  cohesionSlider = createSlider(0, 2, 1, 0.1);
+  separationSlider = createSlider(0, 2, 2, 0.1);
+  for (let i = 0; i < 50; i++) {
+    flock.push(new Boid());
+  }
 }
 
 function draw() {
   background(20);
-
-  for(let boid of flock) {
-    boid.edges();
-    boid.align(flock);
-    boid.update();
-    boid.show();
-  }
+  
+    for (let boid of flock) {
+      boid.edges();
+      boid.flock(flock);
+      boid.update();
+      boid.show();
+    }
 }
-
